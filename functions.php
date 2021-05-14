acffc7f9ebee528787977d7394393d6d95047500
Jelena <jejazz@gmail.com>
2021-05-14 19:27:01 +0200

    sd

--- functions.php
+++ functions.php
@@ -1,5 +1,11 @@
 <?php
-add_action('after_setup_theme', function(){
-    add_image_size('square', 200, 200, false);
-})
+add_action('after_setup_theme', function () {
+    add_image_size('small', 10, 10, true);
+    add_image_size('dd', 14, 14, true);
+});
 
+
+function my()
+{
+    echo "my";
+}
